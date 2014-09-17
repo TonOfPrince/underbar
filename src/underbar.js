79a9a36e0549849fba0a81812ffbd559e1551742
Brett Leibowitz <brettleibo@gmail.com>
2014-09-17 02:09:52 -0500

    last .last() added

--- src/underbar.js
+++ src/underbar.js
@@ -38,7 +38,7 @@ var _ = {};
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
-    return n === undefined ? array[array.length] : array.slice(n, array.length);
+    return n === undefined ? array[array.length()] : array.slice(n, array.length());
   };
 
   // Call iterator(value, key, collection) for each element of collection.
