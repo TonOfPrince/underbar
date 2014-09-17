7a4c5647ea0ac7e2f2921e47cf8d1922de193021
Brett Leibowitz <brettleibo@gmail.com>
2014-09-17 02:17:27 -0500

    last n>length

--- src/underbar.js
+++ src/underbar.js
@@ -38,6 +38,9 @@ var _ = {};
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if n>array.length {
+	n=array.length;
+    }
     return n === undefined ? array[array.length-1] : array.slice(array.length-n, array.length);
   };
 
