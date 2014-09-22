b62680e30b1f1da3e1642751f5d8621db2283588
Brett Leibowitz <brettleibo@gmail.com>
2014-09-21 22:42:29 -0500

    every pass

--- src/underbar.js
+++ src/underbar.js
@@ -222,12 +222,26 @@ var _ = {};
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+	return _.reduce(collection, function(next,item) {
+		if (typeof(iterator) === 'function')  {
+			return next&&Boolean(iterator(item));
+		}
+		else {
+			return next&&Boolean(item);
+		}
+		
+	},true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+	return _.every(collection, iterator);
+	if (iterator=== 'function') {
+	
+	}
+	
   };
 
 
